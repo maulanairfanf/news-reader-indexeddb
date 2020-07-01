7d2bd89209ef8f73024e6c7ce2a6a6be234466fb
maulanairfanf <maulanairfanf@gmail.com>
2020-07-01 10:10:54 +0700

    started

---
+++ service-worker.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = "firstpwa-v2";
+var urlsToCache = [
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/article.html",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/contact.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "/js/api.js",
+    "/js/nav.js"
+];
+
+self.addEventListener("fetch", function(event) {
+    var base_url = "https://readerapi.codepolitan.com/";
+    if (event.request.url.indexOf(base_url) > -1) {
+      event.respondWith(
+        caches.open(CACHE_NAME).then(function(cache) {
+          return fetch(event.request).then(function(response) {
+            cache.put(event.request.url, response.clone());
+            return response;
+          })
+        })
+      );
+    } else {
+        event.respondWith(
+            caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+                return response || fetch (event.request);
+            })
+        )
+    }
+  });
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches
+        .match(event.request, {
+            cacheName: CACHE_NAME
+        })
+        .then(function (response) {
+            if (response) {
+                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                return response;
+            }
+
+            console.log(
+                "ServiceWorker: Memuat aset dari server: ",
+                event.request.url
+            );
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
